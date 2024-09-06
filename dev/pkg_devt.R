d50f08a3ec78bd9e97033054cd0e4da5c126d0dc
Jimmy Briggs <jimmy.briggs@noclocks.dev>
2024-09-06 16:18:31 -0400

    feat: add utils.R

--- dev/pkg_devt.R
+++ dev/pkg_devt.R
@@ -41,3 +41,65 @@ devtools::document()
 usethis::use_testthat()
 usethis::use_test("buildr")
 devtools::check()
+
+lintr::use_lintr()
+usethis::use_build_ignore(".lintr")
+
+usethis::use_coverage()
+
+usethis::use_github_links()
+
+usethis::use_vignette("gmhleasr")
+
+# github actions ----------------------------------------------------------
+
+usethis::use_github_action(
+  name = "document",
+  save_as = "roxygen.yml",
+  badge = TRUE
+)
+
+usethis::use_github_action(
+  name = "lint",
+  save_as = "lint.yml",
+  badge = TRUE
+)
+
+usethis::use_github_action(
+  name = "pr-commands",
+  save_as = "pull-requests.yml",
+  badge = TRUE
+)
+
+usethis::use_github_action(
+  name = "style",
+  save_as = "style.yml",
+  badge = TRUE
+)
+
+usethis::use_github_action(
+  name = "test-coverage",
+  save_as = "coverage.yml",
+  badge = TRUE
+)
+
+usethis::use_github_action(
+  name = "check-standard",
+  save_as = "check.yml",
+  badge = TRUE
+)
+
+usethis::use_pkgdown_github_pages()
+
+
+# functions ---------------------------------------------------------------
+
+c(
+  "utils"
+) |>
+  purrr::walk(usethis::use_test, open = FALSE)
+
+
+usethis::use_vignette("entrata", "Entrata API")
+usethis::use_vignette("decisions", "Architecture Decision Records")
+
